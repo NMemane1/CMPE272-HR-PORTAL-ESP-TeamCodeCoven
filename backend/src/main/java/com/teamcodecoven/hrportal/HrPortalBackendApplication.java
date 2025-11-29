b1de53d8cd0b96379530d9f89783fecc00d20b2e
Nikita Memane <nikitame@Nikitas-MacBook-Pro.local>
2025-11-29 14:42:22 -0800

    Add full Spring Boot backend project structure

---
+++ backend/src/main/java/com/teamcodecoven/hrportal/HrPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teamcodecoven.hrportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPortalBackendApplication.class, args);
+	}
+
+}
